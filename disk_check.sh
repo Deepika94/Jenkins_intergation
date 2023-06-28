d01b837e311840a73eb1379ec65524e53e040400
Deepika94 <44766637+Deepika94@users.noreply.github.com>
2023-06-28 19:55:16 +0530

    Update disk_check.sh

--- disk_check.sh
+++ disk_check.sh
@@ -6,7 +6,7 @@ var=$(df -h | grep '/dev/root'| awk '{print $5 }'| tr -d '%')
 # cmp the disk utlistation 
 if [[ $var -ge $thershold ]];
 then
- echo "Disk is having high utilistation"
+ echo "Disk is having high utilistation on the server"
  # mailbody="Max usage have exceeded" 
  # echo "$mailbody" | mail -s "Disk Alert" "jai.deepika1994@gmail.com"
 elif [[ $var -lt $thershold ]];
