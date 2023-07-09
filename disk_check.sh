8b90263bd46520530f46b3a9d025aa339343ec71
Deepika94 <44766637+Deepika94@users.noreply.github.com>
2023-07-09 12:38:37 +0530

    Update disk_check.sh

--- disk_check.sh
+++ disk_check.sh
@@ -4,12 +4,12 @@ set -e
 thershold=20
 var=$(df -h | grep '/dev/root'| awk '{print $5 }'| tr -d '%')
 # cmp the disk utlistation 
-if [[ $var -ge $thershold ]];
+if [[ $var -ge $thershold ]]
 then
  echo "Disk is having high utilistation on the server "
  # mailbody="Max usage have exceeded" 
  # echo "$mailbody" | mail -s "Disk Alert" "jai.deepika1994@gmail.com"
-elif [[ $var -lt $thershold ]];
+elif [[ $var -lt $thershold ]]
 then
   echo "Disk  space is running under thershold"
 fi
